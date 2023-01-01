41ff54c2fb69f87e151aa9f3615f3e3bab6a33d9
Kaloyan Kostov <kaloyankostov@192.168.0.118>
2023-01-02 00:36:50 +0200

    changed the file to something else

--- ints.java
+++ ints.java
@@ -1,5 +1,35 @@
-public class ints{
-public static void main(String[] args) {
-   System.out.println("tesr"); 
-}
+public class ints {
+   public ints(double initialBalance) {
+      if (initialBalance < 0) {
+         throw new IllegalArgumentException("Initial balance cannot be negative.");
+      }
+      balance = initialBalance;
+   }
+   public void deposit(double amount) {
+      if (amount < 0) {
+         throw new IllegalArgumentException("Cannot deposit a negative amount.");
+      }
+      double newBalance = balance + amount;
+      balance = newBalance;
+   }
+   public void withdraw(double amount) {
+      if (amount < 0 || amount > balance) {
+         throw new IllegalArgumentException("Cannot withdraw an amount greater than the current balance or a negative amount.");
+      }
+      double newBalance = balance - amount;
+      balance = newBalance;
+   }
+   public double getBalance() {
+      return balance;
+   }
+   private double balance;
+   
+   public void catchEmAll() {
+      // Implement this method to catch the IllegalArgumentExceptions and handle 
+
+   }
+
+   public static void main(String[] args) {
+      System.out.println("yikes");
+   }
 }
